034e1d7e4e09a9d4278a51a8b5b2d21af4391a0a
NyxAether <contact.nyxhemera@gmail.com>
2023-12-14 03:37:08 +0100

    some fix

--- advent2023/aoc9/pascal.py
+++ advent2023/aoc9/pascal.py
@@ -1,35 +1,25 @@
 import numpy as np
-from scipy.special import comb
 
 
 class Pascal:
     def __init__(self, values: list[list[int]]) -> None:
-        self.values = np.array(values).astype("int64")
-        self.pascal = {2: np.array([1, -1])}
+        self.values = [np.array(value).astype("int64") for value in values]
 
-    def add_pascal(self, size: int) -> None:
-        self.pascal[size] = np.array(
-            [((-1) ** (i % 2)) * comb(size - 1, i) for i in range(size)]
-        ).astype("int64")
 
-    def get_pascal(self, size: int) -> np.ndarray:
-        if size not in self.pascal:
-            self.add_pascal(size)
-        return self.pascal[size]
 
     def find_pascal(self, series: np.ndarray) -> np.ndarray:
-        n = 2
-        computed_values = [series[-1]]
-        next_val=np.sum(series[-(n):][::-1] * self.get_pascal(n))
-        while (computed_values[-1] != 0 or next_val != 0) and n < len(series):
-            computed_values.append(next_val)
-            next_val =np.sum(series[-(n+1):][::-1] * self.get_pascal(n+1))
-            n += 1
-        return np.array(computed_values[::-1]).astype("int64")
+        
+        current = series.copy()
+        computed_values = [current[-1]]
+        sub=np.array([1,-1])
+        while any(current !=0):
+            current = np.convolve(current,sub)[1:-1]
+            computed_values.append(current[-1])
+        return np.array(computed_values).astype("int64")
 
     def pred_next(self, series: np.ndarray) -> int:
         computed_values = self.find_pascal(series)
         return computed_values.sum()
 
     def compute_sum_preds(self) -> int:
-        return np.sum(np.vectorize(self.pred_next, signature="(i) -> ()")(self.values))
+        return sum([self.pred_next(v) for v in self.values])
--- tests/aoc9/test_pascal.py
+++ tests/aoc9/test_pascal.py
@@ -2,6 +2,14 @@ from advent2023.aoc9.pascal import Pascal
 
 
 def test_compute_sum_preds():
-    assert Pascal([10, 13, 16, 21, 30, 45]).compute_sum_preds() == 68
-    assert Pascal([30,35,41,47,52]).compute_sum_preds() == 55
-    assert Pascal([3,4,6,12,24,43,69]).compute_sum_preds() == 101
+    assert Pascal([[10, 13, 16, 21, 30, 45]]).compute_sum_preds() == 68
+    assert Pascal([[30, 35, 41, 47, 52]]).compute_sum_preds() == 55
+    assert Pascal([[3, 4, 6, 12, 24, 43, 69]]).compute_sum_preds() == 101
+    assert (
+        Pascal(
+            [[10, 13, 16, 21, 30, 45],
+             [30, 35, 41, 47, 52],
+             [3, 4, 6, 12, 24, 43, 69]]
+        ).compute_sum_preds()
+        == 224
+    )
